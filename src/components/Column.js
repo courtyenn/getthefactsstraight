b3d6ca6a28d90f613063b286b9959ebdb28527d4
Courtney Nguyen <cnguyen@1800contacts.com>
2016-05-14 14:43:06 -0600

    upgrading Droptarget

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,7 +1,10 @@
 var React = require("react");
+var ReactDom = require('react-dom');
 // var DropTarget = require("react-dragndrop").DropTarget;
 
 var Column = React.createClass({
+  componentDidMount: function(){
+  },
   getDefaultProps: function(){
     return {
       title: "Default Column Name",
@@ -10,17 +13,14 @@ var Column = React.createClass({
   },
   render: function(){
     return (
-      // <div>
-      //   <DropTarget>
-      //     <h2>{this.props.title}</h2>
-      //     <ul>
-      //       {this.props.children}
-      //     </ul>
-      //   </DropTarget>
-      // </div>
-      <ul>
-        {this.props.children}
-      </ul>
+      <div>
+        <DropTarget>
+          <h2>{this.props.title}</h2>
+          <ul>
+            {this.props.children}
+          </ul>
+        </DropTarget>
+      </div>
     );
   }
 });
