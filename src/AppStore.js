0d9a89ae0afa83bb8f285191852714e117b00619
Courtney Nguyen <courtneythuy@gmail.com>
2016-10-02 11:35:57 -0600

    added task and got rid of excessive code

--- src/AppStore.js
+++ src/AppStore.js
@@ -4,24 +4,6 @@ import CuteData from './cuteData';
 
 CuteData.init();
 
-function deepFreeze(obj) {
-
-  // Retrieve the property names defined on obj
-  var propNames = Object.getOwnPropertyNames(obj);
-
-  // Freeze properties before freezing self
-  propNames.forEach(function(name) {
-    var prop = obj[name];
-
-    // Freeze prop if it is an object
-    if (typeof prop == 'object' && prop !== null)
-      deepFreeze(prop);
-  });
-
-  // Freeze self (no-op if already frozen)
-  return Object.freeze(obj);
-}
-
 const AppStore = Reflux.createStore({
   listenables: [Actions],
   init: function(){
