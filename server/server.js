3eb0b2bb7ecf955131f9625e7edf6c8aef5d2c48
Courtney Nguyen <courtneythuy@gmail.com>
2017-11-22 22:40:17 -0700

    CSS

--- server/server.js
+++ server/server.js
@@ -1,92 +1,97 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
 
-var mongoose = require('mongoose');
-var Quiz = require('./quiz');
-var ejs = require('ejs');
+let mongoose = require('mongoose');
+let Quiz = require('./quiz');
+let ejs = require('ejs');
 let mcache = require('memory-cache')
 mongoose.connect(process.env.DbUrl);
 
-var app = express();
+let app = express();
 app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
 app.use(bodyParser.json());
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './views'));
 
-app.get('/', function (req, res) {
-  console.log('rendering index');
-  res.render('game.html');
+app.get('/', (req, res) => {
+    console.log('rendering index');
+    res.render('game.html');
 });
 
-app.post('/quiz', function (req, res) {
-  console.log('POSTING...');
-  console.log(req.body);
-  console.log(req.body.quiz);
+app.post('/quiz', (req, res) => {
+    console.log('POSTING...');
+    console.log(req.body);
+    console.log(req.body.quiz);
 
-  if (req.query.json) {
-    var quiz = new Quiz();
-    quiz.game = req.body.quiz;
-    quiz.created = Date.now();
-    quiz.title = req.body.title;
-    quiz.save(function (err, data) {
-      res.redirect('/quiz/' + data._id);
-    });
-  }
-  else {
-    var quiz = new Quiz();
-    quiz.columns = req.body.columns;
-    quiz.choices = req.body.choices;
-    quiz.created = Date.now();
-    quiz.title = req.body.title;
-    quiz.save(function (err, data) {
-      res.redirect('/quiz/' + data._id);
-    });
-  }
-
-  // res.render('game.html');
+    if (req.query.json) {
+        var quiz = new Quiz();
+        quiz.game = req.body.quiz;
+        quiz.created = Date.now();
+        quiz.title = req.body.title;
+        quiz.save((err, data) => {
+            res.redirect('/quiz/' + data._id);
+        });
+    }
+    else {
+        var quiz = new Quiz();
+        quiz.columns = req.body.columns;
+        quiz.choices = req.body.choices;
+        quiz.created = Date.now();
+        quiz.title = req.body.title;
+        quiz.save((err, data) => {
+            console.log('should hit here...');
+            console.log(data)
+            if(data && data._id) {
+                res.json({redirect: '/quiz/'+data._id});
+            }
+            else {
+                res.redirect('/');
+            }
+        });
+    }
 });
 
 app.post('/answer', (req, res) => {
     res.json(mcache.get(req.body.answerId) === req.body.columnId);
 });
 
-app.get('/quiz', function (req, res) {
-  res.render('create.html');
+app.get('/quiz', (req, res) => {
+    res.render('create.html');
 });
 
-app.get('/quiz/:id', function (req, res) {
-  console.log('GETTING QUIZ ID', req.params.id);
-  Quiz.findOne({ _id: req.params.id }, function (err, quiz) {
-    let game;
-    var title = quiz.title;
-    if (quiz.game) {
-      game = quiz.game;
-    }
-    else {
-      game = JSON.stringify({ columns: quiz.columns, choices: quiz.choices });
-    }
-    quiz.choices.forEach(answer => {
-        mcache.put(answer.id, answer.correctId);
-        delete answer.correctId;
+app.get('/quiz/:id', (req, res) => {
+    console.log('GETTING QUIZ ID', req.params.id);
+    Quiz.findOne({ _id: req.params.id }, (err, quiz) => {
+        let game;
+        let title = quiz.title;
+
+        if (quiz.game) {
+            game = quiz.game;
+        }
+        else {
+            game = JSON.stringify({ columns: quiz.columns, choices: quiz.choices });
+        }
+
+        quiz.choices.forEach(answer => {
+            mcache.put(answer.id, answer.correctId);
+            delete answer.correctId;
+        });
+
+        console.log(title);
+        res.render('game.html', { game, title });
+    }).lean();
+});
+
+app.get('/quizzes', (req, res) => {
+    Quiz.find({}).sort({ created: 1 }).exec(function (err, data) {
+        res.render('list.html', { list: data });
     });
-
-    console.log(title);
-    res.render('game.html', { game, title });
-  });
 });
 
-app.get('/quizzes', function (req, res) {
-  Quiz.find({}).sort({ created: 1 }).exec(function (err, data) {
-    res.render('list.html', { list: data });
-  });
-});
-
-app.listen(3005, function () {
-  console.log('LISTENING ');
-  console.log(arguments);
+app.listen(3005, () => {
+    console.log('LISTENING ');
 });
--- src/creation/components/CreateGame.js
+++ src/creation/components/CreateGame.js
@@ -3,21 +3,21 @@ import React from 'react';
 import ReactAddons from 'react-addons-update';
 
 import SubList from './Sublist';
-const defaultAnswer = function(id){
+const defaultAnswer = function (id) {
     return {
-      name: 'choice',
-      title: 'Truthy description',
-      edit: false,
-      id: 'answer-' + Math.random(),
-      correctId: id ? id : null
+        name: 'choice',
+        title: 'Truthy description',
+        edit: false,
+        id: 'answer-' + Math.random(),
+        correctId: id ? id : null
     }
 };
-const defaultField = function(){
+const defaultField = function () {
     let field = {
-      name: 'column',
-      title: 'Name column',
-      edit: false,
-      id: 'field-' + Math.random()
+        name: 'column',
+        title: 'Name column',
+        edit: false,
+        id: 'field-' + Math.random()
     };
     field.answers = [defaultAnswer(field.id)];
     return field;
@@ -26,7 +26,7 @@ const defaultField = function(){
 
 export default class CreateGame extends React.Component {
 
-    constructor(){
+    constructor () {
         super();
         this.state = {
             title: 'Common Logic Quiz',
@@ -50,164 +50,187 @@ export default class CreateGame extends React.Component {
         this.removeAnswer = this.removeAnswer.bind(this);
         this.detectKey = this.detectKey.bind(this);
     }
-    createList(){
+
+    createList () {
         let fields = this.state.columns.map((item, i) => {
-          let list = this.createAnswerList(item, i);
-          if(!item.edit){
-            return (
-              <li key={item.id} className="flex-container">
-                <button className="addField" onClick={() => {this.addAnswer(i, item.id)}}></button>
-                <button className="removeField" onClick={() => {this.removeField(i)}}></button>
-                <button className="btn-h" onKeyPress={(e)=> {this.detectKey(e, i)}}><h3 onClick={() => {this.toggleField(i)}}>{item.title}</h3></button>
-                {list}
-              </li>);
-          }
-          else {
-            return (
-              <li key={item.id} className="flex-container">
-                <button className="addField" onClick={() => {this.addAnswer(i, item.id)}}></button>
-                <button className="removeField" onClick={() => {this.removeField(i)}}></button>
-                <button className="btn-h">
-                    <h3><input
-                  autoFocus
-                  className="input-inline"
-                  type="text"
-                  onChange={(e, n)=>{this.updateList(e, item)}}
-                  onKeyPress={(e)=> {this.detectKey(e, i)}}
-                  onBlur={() => {this.toggleField(i)}} placeholder={item.title} />
-                </h3></button>
-                {list}
-              </li>
-            )
-          }
+            let list = this.createAnswerList(item, i);
+            if (!item.edit) {
+                return (
+                    <li key={item.id} className="flex-container">
+                        <div className="list-header flex-container">
+                            <button className="addField" onClick={() => {this.addAnswer(i, item.id)}}></button>
+                            <button className="removeField" onClick={() => {this.removeField(i)}}></button>
+                            <button className="btn-h" onKeyPress={(e)=> {this.detectKey(e, i)}}>
+                                <h3 onClick={() => {this.toggleField(i)}}>{item.title}</h3>
+                            </button>
+                        </div>
+                        {list}
+                    </li>);
+            }
+            else {
+                return (
+                    <li key={item.id} className="flex-container">
+                        <div className="list-header flex-container">
+                            <button className="addField" onClick={() => {this.addAnswer(i, item.id)}}></button>
+                            <button className="removeField" onClick={() => {this.removeField(i)}}></button>
+                            <button className="btn-h">
+                                <h3><input
+                                    autoFocus
+                                    className="input-inline"
+                                    type="text"
+                                    onChange={(e, n)=>{this.updateList(e, item)}}
+                                    onKeyPress={(e)=> {this.detectKey(e, i)}}
+                                    onBlur={() => {this.toggleField(i)}} placeholder={item.title}/>
+                                </h3>
+                            </button>
+                        </div>
+                        {list}
+                    </li>
+                )
+            }
         });
 
         return fields;
-      }
-      detectKey(e, i, j){
+    }
+
+    detectKey (e, i, j) {
         if (e.key === 'Enter') {
-            if(j){
+            if (j) {
                 this.toggleAnswer(i, j);
             }
             else {
                 this.toggleField(i);
             }
-          }
-      }
-      updateList(e, editItem){
+        }
+    }
+
+    updateList (e, editItem) {
         let itemValue = e.target.value;
         let list = this.state.columns;
         list.forEach((item, i) => {
-          if(item.id == editItem.id){
-            list[i].title = itemValue;
-          }
+            if (item.id == editItem.id) {
+                list[i].title = itemValue;
+            }
         });
         let newState = ReactAddons(this.state, {
-          columns: { $set: list }
+            columns: {$set: list}
         });
         this.updateGame(newState);
-      }
-      addField(){
+    }
+
+    addField () {
         let list = this.state.columns;
         let newState = ReactAddons(this.state, {
-          columns: { $push: [defaultField()]}
+            columns: {$push: [defaultField()]}
         });
         this.updateGame(newState);
-      }
-      removeField(i){
+    }
+
+    removeField (i) {
         let list = this.state.columns;
         list.splice(i, 1);
         let newState = ReactAddons(this.state, {
-          columns: { $set: list}
+            columns: {$set: list}
         });
         this.updateGame(newState);
-      }
-      toggleField(i){
+    }
+
+    toggleField (i) {
         let list = this.state.columns;
         list[i].edit = !list[i].edit;
         let newState = ReactAddons(this.state, {
-          columns: { $set: list }
+            columns: {$set: list}
         });
         this.updateGame(newState);
-      }
-      addAnswer(i, correctId){
+    }
+
+    addAnswer (i, correctId) {
         let list = this.state.columns;
         let answer = defaultAnswer(correctId);
         list[i].answers.push(answer);
         let newState = ReactAddons(this.state, {
-          columns: { $set: list }
+            columns: {$set: list}
         });
         this.updateGame(newState);
-      }
-    createAnswerList(fieldColumn, i){
+    }
+
+    createAnswerList (fieldColumn, i) {
         let answers = fieldColumn.answers.map((answer, j) => {
-          if(!answer.edit){
-            return (
-              <li key={answer.id}>
-                <div className="flex-container">
-                  <button className="removeField" onClick={() => {this.removeAnswer(i, j)}}></button>
-                  <button className="btn-h" onKeyPress={(e)=> {this.detectKey(e, i, j)}} onClick={()=> {this.toggleAnswer(i, j)}}><h4 className="description">{answer.title}</h4></button>
-                </div>
-              </li>
-            );
-          }
-          else {
-            return (
-              <li key={answer.id}>
-              <div className="flex-container">
-                  <button className="removeField" onClick={() => {this.removeAnswer(i, j)}}></button>
-                  <button className="btn-h">
-                      <h3><input
-                    className="input-inline"
-                    type="text"
-                    autoFocus
-                    onBlur={()=> {this.toggleAnswer(i, j)}}
-                    onKeyPress={(e) => {this.detectKey(e, i, j)}}
-                    onChange={(e) => {this.updateAnswer(e, answer, i, j)}} /></h3>
-                  </button>
-                </div>
-              </li>
-            );
-          }
+            if (!answer.edit) {
+                return (
+                    <li key={answer.id}>
+                        <div className="flex-container">
+                            <button className="removeField" onClick={() => {this.removeAnswer(i, j)}}></button>
+                            <button className="btn-h" onKeyPress={(e)=> {this.detectKey(e, i, j)}}
+                                    onClick={()=> {this.toggleAnswer(i, j)}}><h4
+                                className="description">{answer.title}</h4></button>
+                        </div>
+                    </li>
+                );
+            }
+            else {
+                return (
+                    <li key={answer.id}>
+                        <div className="flex-container">
+                            <button className="removeField" onClick={() => {this.removeAnswer(i, j)}}></button>
+                            <button className="btn-h">
+                                <h3><input
+                                    className="input-inline"
+                                    type="text"
+                                    autoFocus
+                                    onBlur={()=> {this.toggleAnswer(i, j)}}
+                                    onKeyPress={(e) => {this.detectKey(e, i, j)}}
+                                    onChange={(e) => {this.updateAnswer(e, answer, i, j)}}/></h3>
+                            </button>
+                        </div>
+                    </li>
+                );
+            }
 
         });
         return React.createElement('ul', {className: 'sub-sublist'}, answers);
     }
-    updateName(name){
+
+    updateName (name) {
         let newState = ReactAddons(this.state, {$set: {title: name}});
         this.setState(newState);
     }
-    updateAnswer(e, editItem, i, j){
+
+    updateAnswer (e, editItem, i, j) {
         let itemValue = e.target.value;
         let list = this.state.columns;
         list[i].answers[j].title = itemValue;
         let newState = ReactAddons(this.state, {
-          columns: { $set: list }
+            columns: {$set: list}
         });
         this.updateGame(newState);
     }
-    removeAnswer(i, j){
+
+    removeAnswer (i, j) {
         let list = this.state.columns;
         list[i].answers.splice(j, 1);
         let newState = ReactAddons(this.state, {
-          columns: { $set: list }
+            columns: {$set: list}
         });
         this.updateGame(newState);
     }
-    toggleAnswer(i, j){
+
+    toggleAnswer (i, j) {
         let list = this.state.columns;
         list[i].answers[j].edit = !list[i].answers[j].edit;
         let newState = ReactAddons(this.state, {
-          columns: { $set: list }
+            columns: {$set: list}
         });
         this.updateGame(newState);
     }
-    submitGame(){
+
+    submitGame () {
         axios.post('/quiz', this.state).then(data => {
-            console.log('pOSTED', data);
+            window.location = data.data.redirect;
         });
     }
-    updateGame(newState){
+
+    updateGame (newState) {
         let choices = newState.columns.reduce((columnA, columnB) => {
             console.log('columnB', columnA, columnB);
             return columnA.length > 0 ? columnA.concat(columnB.answers) : columnB.answers;
@@ -215,22 +238,24 @@ export default class CreateGame extends React.Component {
         let finalState = ReactAddons(this.state, {$set: {columns: newState.columns, choices}});
         this.setState(finalState);
     }
-    render(){
+
+    render () {
         let editButton = this.createList();
         return (
-        <div>
-            <h1>Creating a Quiz is Easy...</h1>
-            <label htmlFor="title">Title:</label>
-            <input id="title" type="text" name="title" placeholder={this.state.title} className="input-wide" onChange={(e) => this.updateName(e.target.value)} />
-            <label htmlFor="editor">Create columns to compare and contrast</label>
-            <div className="sublist">
-                <ul>
-                    {editButton}
-                </ul>
-                <button className="btn" type="button" onClick={() => {this.addField()}}>Add column</button>
+            <div>
+                <div className="quiz">
+                    <input id="title" type="text" name="title" placeholder={this.state.title}
+                           className="input-wide title" onChange={(e) => this.updateName(e.target.value)}/>
+                    <label htmlFor="editor">Create columns to compare and contrast</label>
+                    <div className="sublist">
+                        <ul>
+                            {editButton}
+                        </ul>
+                        <button className="btn" type="button" onClick={() => {this.addField()}}>Add column</button>
+                    </div>
+                    <input className="btn primary" type="button" onClick={() => this.submitGame()} value="Submit"/>
+                </div>
             </div>
-            <input className="btn" type="button" onClick={() => this.submitGame()} value="Submit" />
-          </div>
         );
     }
 }
